60dff5ab656420edf64e9f351ddef78da6abeb85
hassan-mehedi <il.commehedihassan2420@gmail.com>
2021-04-22 14:04:12 +0600

    update

---
+++ pracTwo.sql
@@ -0,0 +1,19 @@
+select * from science_class;
+
+insert into science_class values
+(17151001, 'Abu Noman', 80);
+
+insert into science_class
+(enrollment_no, name, science_marks)
+values
+(17151002, 'Abdullah', 85);
+
+insert into science_class
+(enrollment_no, name, science_marks)
+values
+(17151003, 'Mamun', 75),
+(17151004, 'Zahid', 90),
+(17151005, 'Mehedi', 80),
+(17151006, 'Ismail', 85);
+
+
